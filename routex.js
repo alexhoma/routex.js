c386af587c28cb0ecb94bf2980ba2e6de2603d78
alexhoma <alexcm.14@gmail.com>
2019-05-08 22:40:37 +0200

    feat: allows passing object with name property to add() method

--- routex.js
+++ routex.js
@@ -2,10 +2,18 @@ function routex() {
   let routes = [];
 
   function add(route) {
+    if (typeof route !== 'object') {
+      routes.push({
+        name: route,
+        pattern: '/' + route,
+        page: '/' + route
+      });
+    }
+
     routes.push({
-      name: route,
-      pattern: '/' + route,
-      page: '/' + route
+      name: route.name,
+      pattern: '/' + route.name,
+      page: '/' + route.name
     });
 
     return this;
--- routex.test.js
+++ routex.test.js
@@ -1,13 +1,25 @@
 import routex from './routex';
 
 describe('routex', () => {
-  test('add route as string', () => {
-    const { routes } = routex().add('string-route');
+  test('add route name as string', () => {
+    const { routes } = routex().add('route');
     const route = routes[0];
     const expected = {
-      name: 'string-route',
-      pattern: '/string-route',
-      page: '/string-route'
+      name: 'route',
+      pattern: '/route',
+      page: '/route'
+    };
+
+    expect(route).toMatchObject(expected);
+  });
+
+  test('add route name as object property', () => {
+    const { routes } = routex().add({ name: 'route' });
+    const route = routes[0];
+    const expected = {
+      name: 'route',
+      pattern: '/route',
+      page: '/route'
     };
 
     expect(route).toMatchObject(expected);
