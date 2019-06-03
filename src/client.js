e4e3026f6d233075f9470757f0ade1bd3a0366b6
alexhoma <alexcm.14@gmail.com>
2019-06-03 23:04:16 +0200

    feat: returns a composed link when passing a route name

--- src/client.js
+++ src/client.js
@@ -1,17 +1,39 @@
-export default function createRouteLinks(routes) {
-  routes.reduce((acc, route) => {
-    if (!route.name) {
-      throw new Error(`A route name must be defined`);
-    }
+function createRouteLinks(routeDefinitions) {
+  function findRouteByName(routesList, route) {
+    return routesList.find(routeToFind => routeToFind.name === route.name);
+  }
 
-    if (acc.find(routeToFind => routeToFind.name === route.name)) {
-      throw new Error(`This route name is already defined: ${route.name}`);
-    }
+  function createRoute({ name, pattern = name, page = name }) {
+    return {
+      as: '/' + pattern,
+      href: '/' + page
+    };
+  }
 
-    return [...routes, route];
-  }, []);
+  function composeRoutes(routes) {
+    return routes.reduce((accumulatedRoutes, route) => {
+      if (!route.name) {
+        throw new Error(`A route name must be defined`);
+      }
+      if (findRouteByName(accumulatedRoutes, route)) {
+        throw new Error(`This route name is already defined: ${route.name}`);
+      }
+      return [...routes, createRoute(route)];
+    }, []);
+  }
+
+  const composedRoutes = composeRoutes(routeDefinitions);
 
   return {
-    link() {}
+    link({ route }) {
+      const { as, href } = findRouteByName(composedRoutes, route);
+
+      return {
+        as,
+        href
+      };
+    }
   };
 }
+
+export default createRouteLinks;
--- src/client.test.js
+++ src/client.test.js
@@ -2,11 +2,7 @@ import createRouteLinks from './client';
 
 describe('client/createRouteLinks', () => {
   test('should return a link() function', () => {
-    const routes = [
-      {
-        name: 'a-route-name'
-      }
-    ];
+    const routes = [{ name: 'a-route-name' }];
 
     const { link } = createRouteLinks(routes);
 
@@ -14,14 +10,7 @@ describe('client/createRouteLinks', () => {
   });
 
   test('should throw an error if a route name is not defined', () => {
-    const routes = [
-      {
-        name: 'a-route-name'
-      },
-      {
-        pattern: 'a-route-pattern'
-      }
-    ];
+    const routes = [{ name: 'a-route-name' }, { pattern: 'a-route-pattern' }];
 
     expect(() => createRouteLinks(routes)).toThrow(
       new Error(`A route name must be defined`)
@@ -31,16 +20,68 @@ describe('client/createRouteLinks', () => {
   test('should throw an error if a route name is already defined', () => {
     const duplicatedRouteName = 'a-route-name';
     const routes = [
-      {
-        name: duplicatedRouteName
-      },
-      {
-        name: duplicatedRouteName
-      }
+      { name: duplicatedRouteName },
+      { name: duplicatedRouteName }
     ];
 
     expect(() => createRouteLinks(routes)).toThrow(
       new Error(`This route name is already defined: ${duplicatedRouteName}`)
     );
+  });
+
+  describe('link', () => {
+    test('should return link props given a route definition with a name', () => {
+      const routes = [{ name: 'a-route-name' }];
+      const { link } = createRouteLinks(routes);
+
+      const expected = {
+        as: '/a-route-name',
+        href: '/a-route-name'
+      };
+
+      expect(
+        link({
+          route: 'a-route-name'
+        })
+      ).toEqual(expected);
+    });
+
+    test('should return link props given a route definition with a name and pattern', () => {
+      const routes = [{ name: 'a-route-name', pattern: 'a-route-pattern' }];
+      const { link } = createRouteLinks(routes);
+
+      const expected = {
+        as: '/a-route-pattern',
+        href: '/a-route-name'
+      };
+
+      expect(
+        link({
+          route: 'a-route-name'
+        })
+      ).toEqual(expected);
+    });
+
+    test('should return link props given a route definition with a name, pattern and page', () => {
+      const routes = [
+        {
+          name: 'a-route-name',
+          pattern: 'a-route-pattern',
+          page: 'a-route-page'
+        }
+      ];
+      const { link } = createRouteLinks(routes);
+
+      const expected = {
+        as: '/a-route-pattern',
+        href: '/a-route-page'
+      };
+
+      expect(
+        link({
+          route: 'a-route-name'
+        })
+      ).toEqual(expected);
+    });
   });
 });
